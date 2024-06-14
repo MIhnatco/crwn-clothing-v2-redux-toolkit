93ee8304da7db1a71b490fdf20ad75bb99f564db
Ihliana <Ihnatco@protonmail.com>
2024-06-14 10:34:21 +0200

    configureStore and createSlive for user

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,16 +1,29 @@
-import { USER_ACTION_TYPES } from './user.types';
+import { createSlice } from "@reduxjs/toolkit";
 
 const INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
+export const userSlice = createSlice({
+  name: "user",
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCurrentUser(state, action) {
+      state.currentUser = action.payload;
+    },
+  },
+});
 
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      return state;
-  }
-};
+export const { setCurrentUser } = userSlice.actions;
+export const userReducer = userSlice.reducer;
+/*
+It is pretty much a basic value of just current user which we simply set whenever the action of set current 
+user comes through our Redux store. So the flow of data and everything with Redux Toolkit is the same. The 
+actual implementation of Redux does not change. All Redux JS Toolkit does for us is it makes it easier to write Redux 
+code. 
+
+The way we did it before we would write our own reducer and our reducer is just a function that receives the current 
+state as well as the action and then it will return you back an updated state. Inside of our reducer we have this
+switch statement where we check based on the type of the action whether or not we want to respond. If we 
+don't we return the state as is.  IF we do, we return a new state object with the modified values.
+*/
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
   getAuth,
   signInWithRedirect,
@@ -8,7 +8,7 @@ import {
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
-} from 'firebase/auth';
+} from "firebase/auth";
 import {
   getFirestore,
   doc,
@@ -18,15 +18,17 @@ import {
   writeBatch,
   query,
   getDocs,
-} from 'firebase/firestore';
+} from "firebase/firestore";
 
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyA-g_5kB_amO9GZMP1n1m4Rixty-nvUpA8",
+  authDomain: "crw-clothing-db-82ce4.firebaseapp.com",
+  projectId: "crw-clothing-db-82ce4",
+  storageBucket: "crw-clothing-db-82ce4.appspot.com",
+  messagingSenderId: "815004684885",
+  appId: "1:815004684885:web:bf3d80bde17d3774643adc",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -34,7 +36,7 @@ const firebaseApp = initializeApp(firebaseConfig);
 const googleProvider = new GoogleAuthProvider();
 
 googleProvider.setCustomParameters({
-  prompt: 'select_account',
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
@@ -59,11 +61,11 @@ export const addCollectionAndDocuments = async (
   });
 
   await batch.commit();
-  console.log('done');
+  console.log("done");
 };
 
 export const getCategoriesAndDocuments = async () => {
-  const collectionRef = collection(db, 'categories');
+  const collectionRef = collection(db, "categories");
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
@@ -76,7 +78,7 @@ export const createUserDocumentFromAuth = async (
 ) => {
   if (!userAuth) return;
 
-  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userDocRef = doc(db, "users", userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
 
@@ -92,7 +94,7 @@ export const createUserDocumentFromAuth = async (
         ...additionalInformation,
       });
     } catch (error) {
-      console.log('error creating the user', error.message);
+      console.log("error creating the user", error.message);
     }
   }
 
